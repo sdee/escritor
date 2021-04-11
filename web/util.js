340a03d5aae64d6e11c921515be9f0f60b78954e
sdee <thaiyoshi@gmail.com>
2021-04-10 19:43:44 -0700

    fixes export error

---
+++ web/util.js
@@ -0,0 +1,8 @@
+
+const imageUrlBuilder = require('@sanity/image-url')
+import client from "./client";
+const imageBuilder = imageUrlBuilder(client)
+
+export function urlFor(source) {
+  return imageBuilder.image(source)
+}
