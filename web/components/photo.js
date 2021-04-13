09c42397dd60c1200e52e471daa067868e4d2b4d
sdee <thaiyoshi@gmail.com>
2021-04-12 19:37:12 -0700

    clean up and commenting

--- web/components/photo.js
+++ web/components/photo.js
@@ -1,56 +1,44 @@
-import {urlFor} from "../util";
+import { urlFor, joinIfExists } from "../util";
 
-const joinIfExists = (...arr) => arr.filter(x => !!x).join(', ');
-
-const _formatPlace = ({name, _type, location}) => {
-    let placeString = '';
-    const {address, neighborhood, city} = location;
-    let cityString = joinIfExists(neighborhood, city);
-    if (_type==="restaurant" && address) {
-        placeString = `${name} (${[address, cityString].join(', ')})`;
-    }
-    else {
-         placeString = joinIfExists(name, cityString);
-    }
-    return placeString;
-}
+const _formatPlace = ({ name, _type, location }) => {
+  let placeString = "";
+  const { address, neighborhood, city } = location;
+  const cityString = joinIfExists(neighborhood, city);
+  // Example:  El Mercado (Av Hipólito Unanue 203, Miraflores, Lima) 
+  if (_type === "restaurant" && address) {
+    placeString = `${name} (${[address, cityString].join(", ")})`;
+  // Example: Canta Rana, Barranco, Lima
+  } else {
+    placeString = joinIfExists(name, cityString);
+  }
+  return placeString;
+};
 
 export const Caption = ({ caption, place, dish }) => {
-  let formattedCaption = '';
+  let formattedCaption = "";
   if (caption) {
+    // custom caption from data store overrides automated caption
     formattedCaption = caption;
   } else {
-      if (dish && place) {
-        console.log("TYPE", place._type);
-        console.log(place)
-        let locationString = _formatPlace(place);
-        // if (place._type==="restaurant") {
-        //     let restaurant = place.name;
-        //     formattedPlace = [restaurant, locationString].join(', ')
-        // }
-        formattedCaption = `${dish.name} at ${locationString}`
-      } else if (dish) {
-          formattedCaption = dish;
-      }
-        else if (place) {
-            formattedCaption = _formatPlace(place.name, place.location)
-          }
-    
+    // Example: "Lomo Saltado at Danica"
+    if (dish && place) {
+      let locationString = _formatPlace(place);
+      formattedCaption = `${dish.name} at ${locationString}`;
+      // Example: "Lomo Saltado"
+    } else if (dish) {
+      formattedCaption = dish;
+      // Example: "El Mercado, Miraflores, Lima"
+    } else if (place) {
+      formattedCaption = _formatPlace(place.name, place.location);
+    }
   }
   return <i>{formattedCaption}</i>;
 };
 
 export const Photo = ({ photo: { place, image, caption, dish } }) => {
-  console.log(place);
-  console.log(image);
-  console.log(caption);
-  console.log(dish);    
-
-
-
   return (
     <>
-      <img src={urlFor(image).width(600).url()} /> <br />
+      <img src={urlFor(image).width(800).url()} /> <br />
       <Caption caption={caption} dish={dish} place={place} /> <br />
     </>
   );
