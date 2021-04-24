73acc2bf3e27f71a01a7db1f248ef33410f3e676
sdee <thaiyoshi@gmail.com>
2021-04-24 00:05:56 -0700

    About Me base text

---
+++ web/pages/about.js
@@ -0,0 +1,81 @@
+const About = () => {
+  return (
+    <div class="relative py-16 bg-white overflow-hidden mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+      <h1>
+        <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          About Me
+        </span>
+      </h1>
+      <h3>Short Version</h3>
+      <p class="mt-8 text-xl text-gray-500 leading-8">
+        <b>I'm Sutee.</b> This project has been a labor of love for the last few
+        years. I've researched the book over more than four years, enjoying over
+        five hundred meals throughout Peru and countless conversations about
+        food. I learned some Spanish so that I could interview dozens of people
+        from fellow diners to chefs and cooks all over Peru. I took all of the
+        photos you see and wrote all of the articles that you see, using my own
+        research (typos and all). I also wrote the code for this blog (bugs and
+        all).
+      </p>
+      <h3>Long Version</h3>
+      <p class="mt-8 text-xl text-gray-500 leading-8">
+        If you know anything, anything all, about Peruvian names, you’ll know
+        that I’m not Peruvian. However, I ended up spending 10 months
+        researching Peru (about 900 meals) and have been back three more times
+        since.
+      </p>
+      <p class="mt-8 text-xl text-gray-500 leading-8">
+        In a way, my discovery of Peruvian cuisine seems almost accidental. I
+        drunkenly invited myself to Peru years before during an office happy
+        hour while chatting with Carlos, a co-worker who grew up in Cusco. Two
+        years later, I found myself along with Carlos and a couple of other
+        friends on a whirlwind tour of Peru. The our first two weeks in Peru, we
+        were tourists-visiting the Nazca Lines and Machu Picchu. Honestly, food
+        was a bit of an afterthought in our packed two-week itinerary.
+      </p>
+      <p class="mt-8 text-xl text-gray-500 leading-8">
+        Then I stayed behind to learn to cook from Carlos’s mom and step-sister,
+        María. I had quit my job and didn’t know what I wanted to do. I just
+        knew that I wasn’t going to be able to pay Bay Area rent while I figured
+        it out. I wanted break from the abstract world of software and to do
+        something with my hands. My roommate back in San Francisco who was a
+        cook in a real restaurant kitchen dared me to cook in restaurant
+        kitchen.
+      </p>
+      <p class="mt-8 text-xl text-gray-500 leading-8">
+        With Carlos’s help, I was able to find an unpaid kitchen job. I didn’t
+        know any Spanish at the time. While I enjoyed cooking, I was far from a
+        professional as my roommate would often remind me. Suddenly, I found
+        myself helping cook dishes that were exotic-to-me for people who grew up
+        eating the food.
+      </p>
+      <p class="mt-8 text-xl text-gray-500 leading-8">
+        The other cooks were lenient and forgiving when I would confuse two
+        types of potato (to my defense-there are thousands of varieties in Peru)
+        or forget a technique I had thought I mastered the day before. I never
+        truly mastered the two-handed maneuver for peeling tomatoes that
+        everyone in Peru seemed to know, whether they worked in a kitchen or
+        not. When the kitchen wasn’t too busy, the cooks brought me new morsels
+        of food packed with ingredients I’d never before seen to try. I quickly
+        became enamored with Peruvian cuisine.
+      </p>
+      <p class="mt-8 text-xl text-gray-500 leading-8">
+        After just two weeks of Spanish lessons under my belt, I eventually
+        decided to travel around Peru specifically to learn more about the food.
+        I couldn’t just eat all day so I started interviewing chefs with my
+        meager Spanish. My Airbnb hosts, Tom and Paola were extremely supportive
+        and helped me prep for each day’s interview.
+      </p>
+      <p class="mt-8 text-xl text-gray-500 leading-8">
+        Next thing I knew, I found myself traveling to the jungle, then to the
+        largest food festival in Latin America, up the coast of Peru in search
+        of the best ceviche, and finally back to Cusco. My friends joked that I
+        was traveling in a never-ending circle. While I certainly certainly
+        didn’t go the most efficient route, I felt like I was peeling away more
+        and more layers each time.
+      </p>
+    </div>
+  );
+};
+
+export default About;
