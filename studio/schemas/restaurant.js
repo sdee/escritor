f7400847af857a2af62f62f53e8e67ab26f8467c
sdee <thaiyoshi@gmail.com>
2021-04-26 22:18:43 -0700

    adds to try field

--- studio/schemas/restaurant.js
+++ studio/schemas/restaurant.js
@@ -1,17 +1,18 @@
 export default {
-    name: 'restaurant',
-    title: 'Restaurant',
-    type: 'document',
-    fields: [
-        {
-            name: 'name',
-            type: 'Restaurant Name',
-            type: 'string'
-        },
-        {
-            name: 'location',
-            title: 'Location',
-            type: 'location',
-        }
-    ]
-}
+  name: "restaurant",
+  title: "Restaurant",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      type: "Restaurant Name",
+      type: "string",
+    },
+    {
+      name: "location",
+      title: "Location",
+      type: "location",
+    },
+    { name: "toTry", title: "Want to Try", type: "boolean" },
+  ],
+};
