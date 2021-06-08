91fe12db626e5ce1cf8ec88cd6b8e70bb9641b15
sdee <thaiyoshi@gmail.com>
2021-06-07 17:32:24 -0700

    overrides padding for prose and prose-lg

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,7 +2,43 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            figure: {
+              marginTop: '0',
+              marginBottom: '0',
+              paddingTop: '0',
+              paddingBottom: '0',
+            },
+            'figure > *': {
+              marginTop: '0',
+              marginBottom: '0',
+              paddingTop: '0',
+              paddingBottom: '0'
+            },
+          },
+        },
+          "lg": {
+            css: {
+              figure: {
+                marginTop: '0',
+                marginBottom: '0',
+                paddingTop: '0',
+                paddingBottom: '0',
+              },
+              'figure > *': {
+                marginTop: '0',
+                marginBottom: '0',
+                paddingTop: '0',
+                paddingBottom: '0'
+              },
+            },
+          },
+
+    }),
+  },
   },
   variants: {
     extend: {},
