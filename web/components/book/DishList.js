2e4dfc4e876747af4a2b81791bbd32cc30b9b633
sdee <thaiyoshi@gmail.com>
2021-06-07 18:13:29 -0700

    style tweaks

--- web/components/book/DishList.js
+++ web/components/book/DishList.js
@@ -2,13 +2,13 @@ import { Dish } from "./Dish";
 import { Photo } from "../photo";
 export const DishList = ({ title, intro, dishes = [], coverImage }) => {
   return (
-    <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto list-none">
+    <div class="mt-6 prose prose-lg prose-indigo text-gray-500 mx-auto list-none">
       <h2>{title}</h2>
       { coverImage && <Photo photo={coverImage}/> }
       <p class="pb-0">{intro}</p>
-      <div class="list-none divide-y divide-gray-200 py-0">
+      <div class="list-none divide-y divide-gray-200">
         {dishes.map((dish) => (
-          <div class="py-0 px-6" key={dish.name}>
+          <div class="py-1 px-6" key={dish.name}>
             <Dish {...dish} />
           </div>
         ))}
--- web/components/book/RestaurantList.js
+++ web/components/book/RestaurantList.js
@@ -2,13 +2,13 @@ import {RestaurantItem} from "./RestaurantItem";
 import { Photo } from "../photo";
 export const RestaurantList = ({ title, intro, restaurants, coverImage}) => {
   return (
-    <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto list-none">
+    <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto  list-none">
       <h2>{title}</h2>
       { coverImage && <Photo photo={coverImage}/> }
       <p class="pb-0">{intro}</p>
       <div class="list-none divide-y divide-gray-200 py-0">
         {restaurants.map((restaurant) => (
-          <div class="py-1 px-6">
+          <div class="pt-0 px-6 py-1">
             <RestaurantItem {...restaurant} />
           </div>
         ))}
