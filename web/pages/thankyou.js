2e42dfec27404ed2e602a5fc0a69de1d3bfee9dc
sdee <thaiyoshi@gmail.com>
2021-09-11 10:49:11 -0700

    Adds page for stripe to redirect to after order

---
+++ web/pages/thankyou.js
@@ -0,0 +1,57 @@
+const ThankYou = () => {
+  return (
+    <div class="relative py-16 bg-white overflow-hidden">
+      {/* <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+        <div class="relative px-4 sm:px-6 lg:px-8"> */}
+      <div class="text-lg max-w-prose mx-auto">
+        <h1>
+          <span class="mt-2 block text-8xl text-center font-extrabold text-gray-900">
+            Thank you for your purchase!
+          </span>
+        </h1>
+        <p class="mt-10 text-2xl text-gray-500 leading-8">
+          Thank you so much for being one of the first purchasers of my book.
+          You should recieve an email soon with a link to download the pdf. This
+          book will be updated frequently.
+        </p>
+        <p class="mt-10 text-2xl text-gray-500 leading-8">
+          {" "}
+          This has been true labor of love, and your support will help enable me
+          to find new ways to share stories and recommendations about Peruvian
+          food. I hope that one day I will be able to create a print version of
+          this book and find other creative ways to share my passion for
+          all things related to Peruvian culture and cuisine.
+        </p>
+        <p class="mt-10 text-2xl text-gray-500 leading-8">
+          I'm constantly discovering new things about Peruvian food, and I'd
+          like this book to reflect that. Major updates including new chapters
+          will be made 1-2 times year. I will fix typos and outdated information
+          on a rolling basis. The plan is that all updates will be free.
+        </p>
+        <p class="mt-10 text-2xl text-gray-500 leading-8">
+          Other ways to keep in touch:
+          <br />
+          <br />
+          <ul>
+            <li>
+              <b>Instagram: </b> @howtoeatinperu
+            </li>
+            <li>
+              <b>Facebook: </b>
+              <a href="https://www.facebook.com/howtoeatinperu">
+                https://www.facebook.com/howtoeatinperu
+              </a>
+            </li>
+            <li>
+              <b>E-mail: </b>sutee@howtoeatinperu.com
+            </li>
+          </ul>
+        </p>
+      </div>
+    </div>
+    //       </div>
+    //     </div>
+  );
+};
+
+export default ThankYou;
