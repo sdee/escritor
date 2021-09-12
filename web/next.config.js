7785f256651cf0ed3fb85cf6b99ab452b9254b03
sdee <thaiyoshi@gmail.com>
2021-09-11 21:08:34 -0700

    adds thank you page

--- web/next.config.js
+++ web/next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   // Make sure that your node enviroment supports async/await
   exportPathMap: async function (defaultPathMap) {
     // start with basic static routes
-    const pathMap = { "/": { page: "/" }, "/about": {page: "/about"} }
+    const pathMap = { "/": { page: "/" }, "/about": {page: "/about"}, "/thankyou": {page: "/thankyou"} }
     // now dynamically generate routes
     await client
       .fetch('*[_type == "post"].slug.current')
